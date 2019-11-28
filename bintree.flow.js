84a95ec3873a93eb3f318b2dcc9783d9a7b9bc80
Attila Tóth <muchcoffe@gmail.com>
2019-11-28 11:41:30 +0100

    added flow version

--- bintree.flow.js
+++ bintree.flow.js
@@ -1,19 +1,20 @@
-interface BinTree<T> {
-  node: T
-  left?: BinTree<T>
-  right?: BinTree<T>
+// @flow strict
+export interface BinTree<T> {
+  node: T;
+  left?: ?BinTree<T>;
+  right?: ?BinTree<T>;
 }
 
 export type TreeCutter<T, U> = { [k: string]: [T, U] }
 
-export const binTreeBuilder = <T extends {}, U = any, V = any>(
+export const binTreeBuilder = <T: {}, U = any, V = any>(
   cutters: TreeCutter<U, V>[], // properties used for cutting
   prevNode: T,
   cutterParam?: { [k: string]: U | V }
 ): BinTree<T> | null => {
   if (cutters.length < 1) return null
   const [[k, v]] = Object.entries(cutterParam ?? [null, null])
-  const [[newKey, [v1, v2]]] = Object.entries(cutters[0])
+  const [[newKey, [v1, v2]]] = ((Object.entries(cutters[0]): any): [[string, [U | V, U | V]]])
   const node = { ...prevNode, ...(v ? { [k]: v } : {}) }
   return {
     node,
---
+++ example/bin-tree-builder-example.ts
@@ -0,0 +1,14 @@
+import { binTreeBuilder, TreeCutter } from '../bintree'
+
+type ValueTypes = string | number
+
+const cutters: TreeCutter<ValueTypes, ValueTypes>[] = [
+  { type: ['Model S', 'Model 3'] },
+  { '0-60': [2.4, 3.2] },
+  { range: [373, 322] },
+  { drive: ['AWD', 'AWD'] },
+  { color: ['black', 'blue'] }
+]
+const tree = binTreeBuilder(cutters, { manufacturer: 'Tesla' })
+
+console.info(JSON.stringify(tree, null, 2))
