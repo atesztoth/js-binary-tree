84ca0963b8555b1e52622b3a1289ef7fbc325d71
Attila Tóth <muchcoffe@gmail.com>
2019-11-28 17:49:28 +0100

    fix

--- src/ts/bintree.ts
+++ src/ts/bintree.ts
@@ -4,20 +4,20 @@ export interface BinTree<T> {
   right?: BinTree<T>
 }
 
-export type TreeCutter<T, U> = { [k: string]: [T, U] }
+export type TreeCutter<T, U> = { [k: string]: [T | null, U | null] }
 
 export const rememberingTree = <T extends {}, U = any, V = any>(
   cutters: TreeCutter<U, V>[], // properties used for cutting
   prevNode: T,
-  cutterParam?: { [k: string]: U | V }
+  cutterParam?: { [k: string]: U | V | null }
 ): BinTree<T> | null => {
   const [[k, v]] = Object.entries(cutterParam ?? [null, null])
   const node = { ...prevNode, ...(v ? { [k]: v } : {}) }
   if (cutters.length < 1) return { node }
-  const [[newKey, [v1, v2]]] = Object.entries(cutters[0])
-  return {
-    node,
-    left: rememberingTree(cutters.slice(1), node, { [newKey]: v1 }) ?? undefined,
-    right: rememberingTree(cutters.slice(1), node, { [newKey]: v2 }) ?? undefined
-  }
+  const [[newKey, [v1, v2]]] = Object.entries(cutters[0]) // [ [string, [v1, v2] ]
+  return Object.assign(
+    { node },
+    v1 ? { left: rememberingTree(cutters.slice(1), node, { [newKey]: v1 }) ?? undefined } : {},
+    v2 ? { right: rememberingTree(cutters.slice(1), node, { [newKey]: v2 }) ?? undefined } : {}
+  )
 }
--- test/test.ts
+++ test/test.ts
@@ -35,4 +35,39 @@ describe('BinTree', () => {
 
     expect(JSON.stringify(tree)).toEqual(JSON.stringify(target))
   })
+
+  it('constructing a small tree with unequal height subtrees', () => {
+    const cutters: TreeCutter<SampleTypes, SampleTypes>[] = [{ color: ['red', null] }]
+    const tree = rememberingTree(cutters, { manufacturer: 'Tesla' })
+    expect(JSON.stringify(tree)).toEqual(
+      JSON.stringify({
+        node: { manufacturer: 'Tesla' },
+        left: {
+          node: {
+            manufacturer: 'Tesla',
+            color: 'red'
+          }
+        }
+      })
+    )
+  })
+
+  it('constructing an tree stopping one half', () => {
+    const cutters: TreeCutter<SampleTypes, SampleTypes>[] = [
+      { glass: [null, 'will break'] }, // hehe 😈
+      { like: ['yes', 'no'] }
+    ]
+    const tree = rememberingTree(cutters, { manufacturer: 'Tesla' })
+
+    const target: BinTree<{ [k: string]: string }> = {
+      node: { manufacturer: 'Tesla' },
+      right: {
+        node: { manufacturer: 'Tesla', glass: 'will break' },
+        left: { node: { manufacturer: 'Tesla', glass: 'will break', like: 'yes' } },
+        right: { node: { manufacturer: 'Tesla', glass: 'will break', like: 'no' } }
+      }
+    }
+
+    expect(JSON.stringify(tree)).toEqual(JSON.stringify(target))
+  })
 })
